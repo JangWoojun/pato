3d1d18e12cfe9a628b2e2a9b9f527756ec0e1300
JangWoojun <woojun0107@naver.com>
2024-02-03 03:33:22 +0900

    feat: finishButton 클릭 시 MainActivity로 이동 추가

--- app/src/main/java/com/woojun/pato/auth/ProfileActivity.kt
+++ app/src/main/java/com/woojun/pato/auth/ProfileActivity.kt
@@ -1,8 +1,10 @@
 package com.woojun.pato.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import com.woojun.pato.MainActivity
 import com.woojun.pato.R
 import com.woojun.pato.databinding.ActivityProfileBinding
 
@@ -16,6 +18,11 @@ class ProfileActivity : AppCompatActivity() {
         val statusBarColor = ContextCompat.getColor(this, R.color.blue)
         window.statusBarColor = statusBarColor
 
+        // TODO 필수 필드를 만족해야 이동하도록 수정
 
+        binding.finishButton.setOnClickListener {
+            startActivity(Intent(this@ProfileActivity, MainActivity::class.java))
+            finishAffinity()
+        }
     }
 }

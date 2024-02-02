949cdabc46208f8d969247a22337a44e93aaa648
JangWoojun <woojun0107@naver.com>
2024-02-03 00:13:01 +0900

    feat: SignUpActivity 추가

---
+++ app/src/main/java/com/woojun/pato/auth/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.woojun.pato.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.woojun.pato.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}

f6b591485efe6160f1e9547a7be38ac94636f3be
JangWoojun <woojun0107@naver.com>
2024-02-02 09:25:09 +0900

    feat: LoginActivity Binding 설정

--- app/src/main/java/com/woojun/pato/auth/LoginActivity.kt
+++ app/src/main/java/com/woojun/pato/auth/LoginActivity.kt
@@ -2,11 +2,13 @@ package com.woojun.pato.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.woojun.pato.R
+import com.woojun.pato.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

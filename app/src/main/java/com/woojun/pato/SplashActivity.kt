636b066c9199a09f2a5c0a3e4bb0340e5f094bb4
JangWoojun <woojun0107@naver.com>
2024-02-02 09:24:10 +0900

    feat: 임시 SplashActivity 로직 추가

--- app/src/main/java/com/woojun/pato/SplashActivity.kt
+++ app/src/main/java/com/woojun/pato/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.woojun.pato
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.woojun.pato.auth.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // TODO 로그인 확인 후 Login 혹은 Main으로 넘기기 구현
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            finish()
+        }, 1000)
     }
 }

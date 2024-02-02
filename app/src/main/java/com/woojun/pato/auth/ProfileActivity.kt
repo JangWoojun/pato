464a6ee2d6c60b23c5feb8b32ef1e739dfd09ab1
JangWoojun <woojun0107@naver.com>
2024-02-03 02:11:00 +0900

    feat: ProfileActivity binding 설정

--- app/src/main/java/com/woojun/pato/auth/ProfileActivity.kt
+++ app/src/main/java/com/woojun/pato/auth/ProfileActivity.kt
@@ -3,10 +3,13 @@ package com.woojun.pato.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.woojun.pato.R
+import com.woojun.pato.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

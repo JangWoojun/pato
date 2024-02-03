44774f125d958ded814f718628fe5177aa2df21e
JangWoojun <woojun0107@naver.com>
2024-02-04 03:03:11 +0900

    feat: HiddenAdapter 연결 및 목업 데이터 추, backButton 기능 추가

--- app/src/main/java/com/woojun/pato/chat/HiddenFragment.kt
+++ app/src/main/java/com/woojun/pato/chat/HiddenFragment.kt
@@ -5,11 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.woojun.pato.R
 import com.woojun.pato.databinding.FragmentHiddenBinding
 
 class HiddenFragment : Fragment() {
     private var _binding: FragmentHiddenBinding? = null
     private val binding get() = _binding!!
+    private val list = mutableListOf(
+        Hidden(
+            "",
+            "최수빈(24)",
+            "5km 내외"
+        ),
+        Hidden(
+            "",
+            "최수빈(24)",
+            "5km 내외"
+        ),
+        Hidden(
+            "",
+            "최수빈(24)",
+            "5km 내외"
+        ),
+        Hidden(
+            "",
+            "최수빈(24)",
+            "5km 내외"
+        ),
+        Hidden(
+            "",
+            "최수빈(24)",
+            "5km 내외"
+        ),
+        Hidden(
+            "",
+            "최수빈(24)",
+            "5km 내외"
+        ),
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -24,6 +60,18 @@ class HiddenFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.userRecycler.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.userRecycler.adapter = HiddenAdapter(list)
+
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(
+                R.id.chatFragment, null,
+                NavOptions.Builder()
+                    .setPopUpTo(R.id.bottom_navigation_graph, false)
+                    .build())
+        }
+
+
     }
 
     override fun onDestroyView() {

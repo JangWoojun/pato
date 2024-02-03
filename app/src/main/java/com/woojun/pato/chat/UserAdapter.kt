2de6fd5d7bd47335e1a2407b97a85267d2dcd23e
JangWoojun <woojun0107@naver.com>
2024-02-04 02:10:55 +0900

    feat: UserAdapter 추가

---
+++ app/src/main/java/com/woojun/pato/chat/UserAdapter.kt
@@ -0,0 +1,38 @@
+package com.woojun.pato.chat
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.woojun.pato.databinding.UserItemBinding
+
+class UserAdapter(private val userList: MutableList<User>): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val binding = UserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserViewHolder(binding).also { handler->
+            binding.apply {
+
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(userList[position])
+    }
+
+    class UserViewHolder(private val binding: UserItemBinding):
+        ViewHolder(binding.root) {
+        fun bind(user: User) {
+            binding.imageView
+            binding.nameText.text = user.name
+            binding.timeText.text = user.time
+            binding.lastChatText.text = user.lastChat
+        }
+    }
+
+}

dc02acd8b9371a96a8a912573ec58c738622108c
JangWoojun <woojun0107@naver.com>
2024-02-04 03:01:07 +0900

    design: ChattingFragment 스테이터스 바 하얀색으로 변경

--- app/src/main/java/com/woojun/pato/chat/ChattingFragment.kt
+++ app/src/main/java/com/woojun/pato/chat/ChattingFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.woojun.pato.R
 import com.woojun.pato.databinding.FragmentChattingBinding
 
 class ChattingFragment : Fragment() {
@@ -25,6 +27,8 @@ class ChattingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
+        requireActivity().window.statusBarColor = statusBarColor
     }
 
     override fun onDestroyView() {

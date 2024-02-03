2e8f11764abcef41027dc18b9f2a73116628411d
JangWoojun <woojun0107@naver.com>
2024-02-04 00:43:22 +0900

    feat: Bottom Navigation 이동 설정

--- app/src/main/java/com/woojun/pato/MainActivity.kt
+++ app/src/main/java/com/woojun/pato/MainActivity.kt
@@ -2,6 +2,9 @@ package com.woojun.pato
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavOptions
+import androidx.navigation.findNavController
 import com.woojun.pato.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,61 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.storeFragment -> {
+                    binding.bottomMenu1Selected.visibility = View.VISIBLE
+                    binding.bottomMenu1.visibility = View.GONE
+
+                    binding.bottomMenu2Selected.visibility = View.GONE
+                    binding.bottomMenu2.visibility = View.VISIBLE
+
+                    binding.bottomMenu3Selected.visibility = View.GONE
+                    binding.bottomMenu3.visibility = View.VISIBLE
+                }
+                R.id.chatFragment -> {
+                    binding.bottomMenu1Selected.visibility = View.GONE
+                    binding.bottomMenu1.visibility = View.VISIBLE
+
+                    binding.bottomMenu2Selected.visibility = View.VISIBLE
+                    binding.bottomMenu2.visibility = View.GONE
+
+                    binding.bottomMenu3Selected.visibility = View.GONE
+                    binding.bottomMenu3.visibility = View.VISIBLE
+                }
+                R.id.myInfoFragment -> {
+                    binding.bottomMenu1Selected.visibility = View.GONE
+                    binding.bottomMenu1.visibility = View.VISIBLE
+
+                    binding.bottomMenu2Selected.visibility = View.GONE
+                    binding.bottomMenu2.visibility = View.VISIBLE
+
+                    binding.bottomMenu3Selected.visibility = View.VISIBLE
+                    binding.bottomMenu3.visibility = View.GONE
+                }
+            }
+        }
+
+        binding.store.setOnClickListener {
+            navController.navigate(R.id.storeFragment, null,
+                NavOptions.Builder()
+                    .setPopUpTo(R.id.chatFragment, false)
+                    .build())
+        }
+        binding.chat.setOnClickListener {
+            navController.navigate(R.id.chatFragment, null,
+                NavOptions.Builder()
+                    .setPopUpTo(R.id.chatFragment, false)
+                    .build())
+        }
+        binding.myInfo.setOnClickListener {
+            navController.navigate(R.id.myInfoFragment, null,
+                NavOptions.Builder()
+                    .setPopUpTo(R.id.chatFragment, false)
+                    .build())
+        }
+
     }
 }

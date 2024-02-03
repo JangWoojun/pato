0e79c05e93fe4c874918eb27e1f27b5fb2fa3a85
JangWoojun <woojun0107@naver.com>
2024-02-04 02:18:36 +0900

    feat: ChatFragment 하위 Fragments 추가 및 binding 설정

---
+++ app/src/main/java/com/woojun/pato/chat/HiddenFragment.kt
@@ -0,0 +1,34 @@
+package com.woojun.pato.chat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woojun.pato.databinding.FragmentHiddenBinding
+
+class HiddenFragment : Fragment() {
+    private var _binding: FragmentHiddenBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHiddenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}

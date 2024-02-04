44389014c48d13fab56e8e0eb4cd756a3dc9aeca
JangWoojun <woojun0107@naver.com>
2024-02-05 07:28:04 +0900

    feat: StoreAdapter 연결 및 목업 데이터 추가

--- app/src/main/java/com/woojun/pato/store/StoreFragment.kt
+++ app/src/main/java/com/woojun/pato/store/StoreFragment.kt
@@ -5,12 +5,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.woojun.pato.databinding.FragmentStoreBinding
 
 class StoreFragment : Fragment() {
 
     private var _binding: FragmentStoreBinding? = null
     private val binding get() = _binding!!
+    private val list = mutableListOf(
+        Store(
+            "티모 대위",
+            "30,900원",
+            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160311_185%2Ftryandplay_1457674689918P9j4h_JPEG%2F02.JPG&type=a340",
+            "https://smartstore.naver.com/unomarket_kr/products/9402668962?NaPm=ct%3Dls82bmxk%7Cci%3Dfdfe7ca506c1dfbcc337df0f296ecf937b59af35%7Ctr%3Dslsl%7Csn%3D9092384%7Chk%3D602513aa69e3ef3a46341598a77cc0ae7d7aa781"
+        ),
+        Store(
+            "티모 대위",
+            "30,900원",
+            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160311_185%2Ftryandplay_1457674689918P9j4h_JPEG%2F02.JPG&type=a340",
+            "https://smartstore.naver.com/unomarket_kr/products/9402668962?NaPm=ct%3Dls82bmxk%7Cci%3Dfdfe7ca506c1dfbcc337df0f296ecf937b59af35%7Ctr%3Dslsl%7Csn%3D9092384%7Chk%3D602513aa69e3ef3a46341598a77cc0ae7d7aa781"
+        ),
+        Store(
+            "티모 대위",
+            "30,900원",
+            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160311_185%2Ftryandplay_1457674689918P9j4h_JPEG%2F02.JPG&type=a340",
+            "https://smartstore.naver.com/unomarket_kr/products/9402668962?NaPm=ct%3Dls82bmxk%7Cci%3Dfdfe7ca506c1dfbcc337df0f296ecf937b59af35%7Ctr%3Dslsl%7Csn%3D9092384%7Chk%3D602513aa69e3ef3a46341598a77cc0ae7d7aa781"
+        ),
+        Store(
+            "티모 대위",
+            "30,900원",
+            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160311_185%2Ftryandplay_1457674689918P9j4h_JPEG%2F02.JPG&type=a340",
+            "https://smartstore.naver.com/unomarket_kr/products/9402668962?NaPm=ct%3Dls82bmxk%7Cci%3Dfdfe7ca506c1dfbcc337df0f296ecf937b59af35%7Ctr%3Dslsl%7Csn%3D9092384%7Chk%3D602513aa69e3ef3a46341598a77cc0ae7d7aa781"
+        ),
+        Store(
+            "티모 대위",
+            "30,900원",
+            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160311_185%2Ftryandplay_1457674689918P9j4h_JPEG%2F02.JPG&type=a340",
+            "https://smartstore.naver.com/unomarket_kr/products/9402668962?NaPm=ct%3Dls82bmxk%7Cci%3Dfdfe7ca506c1dfbcc337df0f296ecf937b59af35%7Ctr%3Dslsl%7Csn%3D9092384%7Chk%3D602513aa69e3ef3a46341598a77cc0ae7d7aa781"
+        ),
+        Store(
+            "티모 대위",
+            "30,900원",
+            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160311_185%2Ftryandplay_1457674689918P9j4h_JPEG%2F02.JPG&type=a340",
+            "https://smartstore.naver.com/unomarket_kr/products/9402668962?NaPm=ct%3Dls82bmxk%7Cci%3Dfdfe7ca506c1dfbcc337df0f296ecf937b59af35%7Ctr%3Dslsl%7Csn%3D9092384%7Chk%3D602513aa69e3ef3a46341598a77cc0ae7d7aa781"
+        ),
+        Store(
+            "티모 대위",
+            "30,900원",
+            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160311_185%2Ftryandplay_1457674689918P9j4h_JPEG%2F02.JPG&type=a340",
+            "https://smartstore.naver.com/unomarket_kr/products/9402668962?NaPm=ct%3Dls82bmxk%7Cci%3Dfdfe7ca506c1dfbcc337df0f296ecf937b59af35%7Ctr%3Dslsl%7Csn%3D9092384%7Chk%3D602513aa69e3ef3a46341598a77cc0ae7d7aa781"
+        ),
+        Store(
+            "티모 대위",
+            "30,900원",
+            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160311_185%2Ftryandplay_1457674689918P9j4h_JPEG%2F02.JPG&type=a340",
+            "https://smartstore.naver.com/unomarket_kr/products/9402668962?NaPm=ct%3Dls82bmxk%7Cci%3Dfdfe7ca506c1dfbcc337df0f296ecf937b59af35%7Ctr%3Dslsl%7Csn%3D9092384%7Chk%3D602513aa69e3ef3a46341598a77cc0ae7d7aa781"
+        ),
+        Store(
+            "티모 대위",
+            "30,900원",
+            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160311_185%2Ftryandplay_1457674689918P9j4h_JPEG%2F02.JPG&type=a340",
+            "https://smartstore.naver.com/unomarket_kr/products/9402668962?NaPm=ct%3Dls82bmxk%7Cci%3Dfdfe7ca506c1dfbcc337df0f296ecf937b59af35%7Ctr%3Dslsl%7Csn%3D9092384%7Chk%3D602513aa69e3ef3a46341598a77cc0ae7d7aa781"
+        ),
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -25,6 +82,9 @@ class StoreFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.storeRecycler.layoutManager = GridLayoutManager(requireContext(), 3)
+        binding.storeRecycler.adapter = StoreAdapter(list)
     }
 
     override fun onDestroyView() {

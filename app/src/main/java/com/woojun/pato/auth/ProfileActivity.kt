e4fced45f5a769a46b33bf9fbffc63118850a40e
JangWoojun <woojun0107@naver.com>
2024-02-14 13:05:24 +0900

    remove: 삭제 된 기능 임시 제거

--- app/src/main/java/com/woojun/pato/auth/ProfileActivity.kt
+++ app/src/main/java/com/woojun/pato/auth/ProfileActivity.kt
@@ -65,7 +65,6 @@ class ProfileActivity : AppCompatActivity() {
             getContent.launch("image/*")
         }
 
-        binding.finishButton.isEnabled = false
 
         binding.nicknameInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {
@@ -81,22 +80,22 @@ class ProfileActivity : AppCompatActivity() {
             }
         })
 
-        binding.ageBox.setOnClickListener {
-            showYearPickerDialog()
-            isFinish()
-        }
-
-        binding.manButton.setOnClickListener {
-            it.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#5666FF"))
-            binding.womanButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#C4C4C4"))
-            sex = "남성"
-        }
-
-        binding.womanButton.setOnClickListener {
-            it.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#5666FF"))
-            binding.manButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#C4C4C4"))
-            sex = "여성"
-        }
+//        binding.ageBox.setOnClickListener {
+//            showYearPickerDialog()
+//            isFinish()
+//        }
+//
+//        binding.manButton.setOnClickListener {
+//            it.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#5666FF"))
+//            binding.womanButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#C4C4C4"))
+//            sex = "남성"
+//        }
+//
+//        binding.womanButton.setOnClickListener {
+//            it.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#5666FF"))
+//            binding.manButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#C4C4C4"))
+//            sex = "여성"
+//        }
 
         val layoutInflater = LayoutInflater.from(this@ProfileActivity)
         val popupView = layoutInflater.inflate(R.layout.dropdown_menu, null)
@@ -107,101 +106,101 @@ class ProfileActivity : AppCompatActivity() {
             true
         )
 
-        binding.locationBox.setOnClickListener {
-            popupWindow.showAsDropDown(it)
-        }
-
-        popupView.findViewById<CardView>(R.id.gangnam_box).setOnClickListener {
-            binding.locationInput.text = "서울특별시 강남구"
-            binding.locationInput.setTextColor(Color.parseColor("#5666FF"))
-            popupWindow.dismiss()
-            isFinish()
-        }
-
-        popupView.findViewById<CardView>(R.id.gangbuk_box).setOnClickListener {
-            binding.locationInput.text = "서울특별시 강북구"
-            binding.locationInput.setTextColor(Color.parseColor("#5666FF"))
-            popupWindow.dismiss()
-            isFinish()
-        }
-
-        popupView.findViewById<CardView>(R.id.gangdong_box).setOnClickListener {
-            binding.locationInput.text = "서울특별시 강동구"
-            binding.locationInput.setTextColor(Color.parseColor("#5666FF"))
-            popupWindow.dismiss()
-            isFinish()
-        }
-
-        popupView.findViewById<CardView>(R.id.gangseo_box).setOnClickListener {
-            binding.locationInput.text = "서울특별시 강서구"
-            binding.locationInput.setTextColor(Color.parseColor("#5666FF"))
-            popupWindow.dismiss()
-            isFinish()
-        }
-
-        popupView.findViewById<CardView>(R.id.gwanak_box).setOnClickListener {
-            binding.locationInput.text = "서울특별시 관악구"
-            binding.locationInput.setTextColor(Color.parseColor("#5666FF"))
-            popupWindow.dismiss()
-            isFinish()
-        }
-
-        binding.finishButton.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                val db = AppDatabase.getDatabase(this@ProfileActivity)
-                val user = User(
-                    getUid(),
-                    profileImageUrl,
-                    binding.nicknameInput.text.toString(),
-                    date,
-                    sex,
-                    binding.locationInput.text.toString()
-                )
-                db?.userDao()!!.insertUser(user)
-                withContext(Dispatchers.Main) {
-                    startActivity(Intent(this@ProfileActivity, MainActivity::class.java))
-                    finishAffinity()
-                }
-            }
-        }
+//        binding.locationBox.setOnClickListener {
+//            popupWindow.showAsDropDown(it)
+//        }
+//
+//        popupView.findViewById<CardView>(R.id.gangnam_box).setOnClickListener {
+//            binding.locationInput.text = "서울특별시 강남구"
+//            binding.locationInput.setTextColor(Color.parseColor("#5666FF"))
+//            popupWindow.dismiss()
+//            isFinish()
+//        }
+//
+//        popupView.findViewById<CardView>(R.id.gangbuk_box).setOnClickListener {
+//            binding.locationInput.text = "서울특별시 강북구"
+//            binding.locationInput.setTextColor(Color.parseColor("#5666FF"))
+//            popupWindow.dismiss()
+//            isFinish()
+//        }
+//
+//        popupView.findViewById<CardView>(R.id.gangdong_box).setOnClickListener {
+//            binding.locationInput.text = "서울특별시 강동구"
+//            binding.locationInput.setTextColor(Color.parseColor("#5666FF"))
+//            popupWindow.dismiss()
+//            isFinish()
+//        }
+//
+//        popupView.findViewById<CardView>(R.id.gangseo_box).setOnClickListener {
+//            binding.locationInput.text = "서울특별시 강서구"
+//            binding.locationInput.setTextColor(Color.parseColor("#5666FF"))
+//            popupWindow.dismiss()
+//            isFinish()
+//        }
+//
+//        popupView.findViewById<CardView>(R.id.gwanak_box).setOnClickListener {
+//            binding.locationInput.text = "서울특별시 관악구"
+//            binding.locationInput.setTextColor(Color.parseColor("#5666FF"))
+//            popupWindow.dismiss()
+//            isFinish()
+//        }
+//
+//        binding.finishButton.setOnClickListener {
+//            CoroutineScope(Dispatchers.IO).launch {
+//                val db = AppDatabase.getDatabase(this@ProfileActivity)
+//                val user = User(
+//                    getUid(),
+//                    profileImageUrl,
+//                    binding.nicknameInput.text.toString(),
+//                    date,
+//                    sex,
+//                    binding.locationInput.text.toString()
+//                )
+//                db?.userDao()!!.insertUser(user)
+//                withContext(Dispatchers.Main) {
+//                    startActivity(Intent(this@ProfileActivity, MainActivity::class.java))
+//                    finishAffinity()
+//                }
+//            }
+//        }
     }
 
     private fun showYearPickerDialog() {
         val calendar = Calendar.getInstance()
         val currentYear = calendar.get(Calendar.YEAR)
 
-        val datePickerDialog = DatePickerDialog(
-            this,
-            { _, selectedYear, _, _ ->
-                date = selectedYear.toString()
-                binding.ageInput.text = "${selectedYear}년생 (만 ${currentYear - selectedYear}세)"
-                binding.ageInput.setTextColor(Color.parseColor("#5666FF"))
-                binding.ageBox.setStrokeColor(ColorStateList.valueOf(Color.parseColor("#5666FF")))
-            },
-            currentYear,
-            0,
-            1
-        )
-
-        datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
-
-        datePickerDialog.datePicker.init(currentYear, 0, 1, null)
-
-        datePickerDialog.show()
+//        val datePickerDialog = DatePickerDialog(
+//            this,
+//            { _, selectedYear, _, _ ->
+//                date = selectedYear.toString()
+//                binding.ageInput.text = "${selectedYear}년생 (만 ${currentYear - selectedYear}세)"
+//                binding.ageInput.setTextColor(Color.parseColor("#5666FF"))
+//                binding.ageBox.setStrokeColor(ColorStateList.valueOf(Color.parseColor("#5666FF")))
+//            },
+//            currentYear,
+//            0,
+//            1
+//        )
+//
+//        datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
+//
+//        datePickerDialog.datePicker.init(currentYear, 0, 1, null)
+//
+//        datePickerDialog.show()
     }
 
     private fun isFinish() {
-        if (
-            binding.nicknameInput.text.isNotEmpty() && binding.ageInput.text != "태어난 연도를 선택해주세요..."
-            && binding.locationInput.text != "지역을 선택해주세요..." && profileImageUrl != ""
-        ) {
-            binding.finishButton.isEnabled = true
-            binding.finishButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#5666FF"))
-        } else {
-            binding.finishButton.isEnabled = false
-            binding.finishButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#C4C4C4"))
-            binding.nicknameBox.setStrokeColor(ColorStateList.valueOf(Color.parseColor("#C4C4C4")))
-        }
+//        if (
+//            binding.nicknameInput.text.isNotEmpty() && binding.ageInput.text != "태어난 연도를 선택해주세요..."
+//            && binding.locationInput.text != "지역을 선택해주세요..." && profileImageUrl != ""
+//        ) {
+//            binding.finishButton.isEnabled = true
+//            binding.finishButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#5666FF"))
+//        } else {
+//            binding.finishButton.isEnabled = false
+//            binding.finishButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#C4C4C4"))
+//            binding.nicknameBox.setStrokeColor(ColorStateList.valueOf(Color.parseColor("#C4C4C4")))
+//        }
     }
 
     private fun uploadImageToFirebaseStorage() {

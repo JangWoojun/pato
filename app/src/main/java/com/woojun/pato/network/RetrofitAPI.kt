c08dc46c5b45eb26e782b9f684fd305d560debb5
JangWoojun <woojun0107@naver.com>
2024-02-21 05:26:28 +0900

    feat: duplicateCheck 추가

--- app/src/main/java/com/woojun/pato/network/RetrofitAPI.kt
+++ app/src/main/java/com/woojun/pato/network/RetrofitAPI.kt
@@ -1,10 +1,14 @@
 package com.woojun.pato.network
 
+import com.woojun.pato.auth.CheckNicknameRequest
+import com.woojun.pato.auth.CheckNicknameResponse
 import com.woojun.pato.auth.JwtToken
 import com.woojun.pato.auth.Profile
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface RetrofitAPI {
@@ -17,4 +21,9 @@ interface RetrofitAPI {
     fun getProfile(
         @Header("Authorization") authorization: String,
     ): Call<Profile>
+
+    @POST("profile/check-nickname")
+    fun duplicateCheck(
+        @Body nickname: CheckNicknameRequest,
+    ): Call<CheckNicknameResponse>
 }

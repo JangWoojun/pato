07898be988fcf30bbed1cb26abad8d97102f7c8d
JangWoojun <woojun0107@naver.com>
2024-02-21 02:52:40 +0900

    feat: Profile DataClass 추가

---
+++ app/src/main/java/com/woojun/pato/auth/Profile.kt
@@ -0,0 +1,10 @@
+package com.woojun.pato.auth
+
+data class Profile(
+    val alcohol: Double,
+    val hobby: String,
+    val image: String,
+    val nickname: String,
+    val region: String,
+    val uuid: String
+)

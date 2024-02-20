6d2f6463aa4f7cc8cf04b3ebf1fed929b5ec56d9
JangWoojun <woojun0107@naver.com>
2024-02-21 02:51:11 +0900

    feat: Preferences 세팅

---
+++ app/src/main/java/com/woojun/pato/auth/AppPreferences.kt
@@ -0,0 +1,28 @@
+package com.woojun.pato.auth
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object AppPreferences {
+    private const val NAME = "AppPreferences"
+    private const val MODE = Context.MODE_PRIVATE
+    private lateinit var preferences: SharedPreferences
+
+    private val TOKEN = Pair("token", "")
+
+    fun init(context: Context) {
+        preferences = context.getSharedPreferences(NAME, MODE)
+    }
+
+    private inline fun SharedPreferences.edit(operation: (SharedPreferences.Editor) -> Unit) {
+        val editor = edit()
+        operation(editor)
+        editor.apply()
+    }
+
+    var token: String
+        get() = preferences.getString(TOKEN.first, TOKEN.second) ?: ""
+        set(value) = preferences.edit {
+            it.putString(TOKEN.first, value)
+        }
+}

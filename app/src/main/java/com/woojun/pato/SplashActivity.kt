4badc3563f2ed0457f9d4d1a6446abd537dfa200
JangWoojun <woojun0107@naver.com>
2024-02-23 08:02:39 +0900

    feat: SplashActivity 이동 코드 수정

--- app/src/main/java/com/woojun/pato/SplashActivity.kt
+++ app/src/main/java/com/woojun/pato/SplashActivity.kt
@@ -1,10 +1,12 @@
 package com.woojun.pato
 
+import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.core.content.ContextCompat
 import com.woojun.pato.auth.AppPreferences
 import com.woojun.pato.auth.LoginActivity
 import com.woojun.pato.auth.ProfileActivity
@@ -14,17 +16,18 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val statusBarColor = ContextCompat.getColor(this, R.color.primary)
+        window.statusBarColor = statusBarColor
+
         Handler(Looper.getMainLooper()).postDelayed({
             if (AppPreferences.profile && AppPreferences.token != "") {
                 startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-                finish()
             } else if (AppPreferences.token != "") {
                 startActivity(Intent(this@SplashActivity, ProfileActivity::class.java))
-                finish()
             } else {
                 startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
-                finish()
             }
-        }, 1000)
+            finish()
+        }, 1500)
     }
 }

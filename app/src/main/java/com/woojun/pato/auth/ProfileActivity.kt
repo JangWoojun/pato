7357eaefa163c84bbdb55207db3f42d6790d588f
JangWoojun <woojun0107@naver.com>
2024-02-24 02:57:49 +0900

    feat: 이미지 선택 시 유저에게 보여주는 기능 추가

--- app/src/main/java/com/woojun/pato/auth/ProfileActivity.kt
+++ app/src/main/java/com/woojun/pato/auth/ProfileActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -19,6 +20,12 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.woojun.pato.MainActivity
 import com.woojun.pato.R
 import com.woojun.pato.databinding.ActivityProfileBinding
@@ -323,6 +330,34 @@ class ProfileActivity : AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
             uri?.let {
                 image = uriToBase64(this@ProfileActivity, it)
+                Glide.with(this)
+                    .load(it)
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            binding.defaultProfileImage.visibility = View.VISIBLE
+                            binding.profileImage.visibility = View.INVISIBLE
+                            return false
+                        }
+                        override fun onResourceReady(
+                            resource: Drawable,
+                            model: Any,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            binding.defaultProfileImage.visibility = View.INVISIBLE
+                            binding.profileImage.visibility = View.VISIBLE
+                            return false
+                        }
+                    })
+                    .into(binding.profileImage)
             }
             isFinish()
         }

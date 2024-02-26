96d0a1dc1df7cfa8813303e09e413ce1db1b147e
JangWoojun <woojun0107@naver.com>
2024-02-27 05:22:03 +0900

    feat: matchingStart 로직 구현

--- app/src/main/java/com/woojun/pato/chat/ChatFragment.kt
+++ app/src/main/java/com/woojun/pato/chat/ChatFragment.kt
@@ -1,14 +1,22 @@
 package com.woojun.pato.chat
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.woojun.pato.R
+import com.woojun.pato.auth.AppPreferences
 import com.woojun.pato.databinding.FragmentChatBinding
+import com.woojun.pato.network.RetrofitAPI
+import com.woojun.pato.network.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ChatFragment : Fragment() {
 
@@ -49,9 +57,7 @@ class ChatFragment : Fragment() {
         }
 
         binding.matchingStartButton.setOnClickListener {
-            binding.loadingBox.visibility = View.VISIBLE
-            binding.readyBox.visibility = View.INVISIBLE
-            binding.setHiddenButton.visibility = View.INVISIBLE
+            matchingStart("Bearer ${AppPreferences.token}")
         }
 
         binding.setHiddenButton.setOnClickListener {
@@ -63,6 +69,34 @@ class ChatFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun matchingWaiting() {
+
+    }
+
+    private fun matchingStart(header: String) {
+        val retrofit = RetrofitClient.getInstance()
+        val apiService = retrofit.create(RetrofitAPI::class.java)
+        val call = apiService.matchingStart(header)
+
+        call.enqueue(object : Callback<Matching> {
+            override fun onResponse(call: Call<Matching>, response: Response<Matching>) {
+                if (response.isSuccessful && response.body()!!.status == "matched") {
+                    startActivity(Intent(requireContext(), ChattingActivity::class.java))
+                } else if (response.body()!!.status == "waiting") {
+                    binding.loadingBox.visibility = View.VISIBLE
+                    binding.readyBox.visibility = View.INVISIBLE
+                    binding.setHiddenButton.visibility = View.INVISIBLE
+                    matchingWaiting()
+                } else {
+                    Toast.makeText(context, "매칭에 실패하였습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onFailure(call: Call<Matching>, t: Throwable) {
+                Toast.makeText(context, "매칭에 실패하였습니다", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
 }
